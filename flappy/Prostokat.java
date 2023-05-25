bc6426e196887f0b9f10a4d1b5e29f25e68ca389
JakubJaroszewski <74415980+JakubJaroszewski@users.noreply.github.com>
2023-05-25 22:17:58 +0200

    Update Prostokat.java

--- flappy/Prostokat.java
+++ flappy/Prostokat.java
@@ -4,18 +4,15 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
-
 public class Prostokat{
-
 	int Velx = 1;
 	int Vely = 1;
 	int y = 0;
 	int x,h; //h  okolo 400
-	
-	 public void paint(Graphics g){
+		public void paint(Graphics g){
 			g.setColor(Color.GREEN);
-	        g.fillRect(x,y,50,h);
-}
+	       		g.fillRect(x,y,50,h);
+		}
 	 void SetX(int d) {
 		 x=d;
 	 }
