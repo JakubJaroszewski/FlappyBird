1ea9cda9cf901d3c837e1bc1c19c0affd5a59a37
JakubJaroszewski <74415980+JakubJaroszewski@users.noreply.github.com>
2023-05-25 22:20:46 +0200

    Update SoundEffect.java

--- flappy/SoundEffect.java
+++ flappy/SoundEffect.java
@@ -1,30 +1,22 @@
 package flappy;
 
 import java.io.File;
-
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class SoundEffect {
-	
 	Clip clip;
-	
 	public void setFile(String soundFileName){
-		
 		try{
 			File file = new File(soundFileName);
 			AudioInputStream sound = AudioSystem.getAudioInputStream(file);	
 			clip = AudioSystem.getClip();
 			clip.open(sound);
 		}
-		catch(Exception e){
-			
-		}
+		catch(Exception e){}
 	}
-	
-	public void play(){
-		
+	public void play(){	
 		clip.setFramePosition(0);
 		clip.start();
 	}
