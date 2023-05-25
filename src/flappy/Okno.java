6ee1ce8753f2b5c34aedd3b6f2085397f29dfdf5
JakubJaroszewski <74415980+JakubJaroszewski@users.noreply.github.com>
2023-05-25 23:49:19 +0200

    Update Okno.java

--- src/flappy/Okno.java
+++ src/flappy/Okno.java
@@ -3,30 +3,10 @@ package flappy;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 
 public class Okno extends JFrame{
-
-	/**
-	 * 
-	 */
-	//private static final long serialVersionUID = 1L;
-
-	public static void main(String[] args) {
-	//	start1 a = new start1();
-		//Flappy gra = new Flappy();
-		//a.setVisible(true);
-		//gra.setVisible(false);
-		// ActionListener b = new ActionListener() {
-	       //    public void actionPerformed(ActionEvent e) {
-	        //	   a.setVisible(false);
-	        	  // gra.setVisible(true);
-	          // }
-	     //  };
-	//  a.button1.addActionListener(b);
-
-	}
+	public static void main(String[] args) {}
 
 }
