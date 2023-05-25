da2a6d6f73f83fb273c1187326f0396f738c7c3a
JakubJaroszewski <74415980+JakubJaroszewski@users.noreply.github.com>
2023-05-25 22:18:25 +0200

    Update Prostokat2.java

--- flappy/Prostokat2.java
+++ flappy/Prostokat2.java
@@ -7,19 +7,17 @@ public class Prostokat2 {
 	int Velx = 1;
 	int Vely = 1;
 	int y = 0;
-	int x,h; // h o 100 mniejsze od h z prostokata 
-	public Prostokat2() {
-	
-	}
+	int x,h;
+	public Prostokat2() {}
 	 public void paint(Graphics g){
-			g.setColor(Color.GREEN);
+		g.setColor(Color.GREEN);
 	        g.fillRect(x,600,50,h);
 	
-}
+	}
 	 void SetX(int d) {
 		 x=d;
-	 }
+	}
 	 void SetH(int d) {
 		 h=d;
-	 }
+	}
 }
